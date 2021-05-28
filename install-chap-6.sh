a568c332d4cb299988a28fd1e75b9d43b5808f23
sbelondr <sbelondr@student.42.fr>
2021-05-28 17:27:11 +0200

    update package chap 6 for lfs 10.1

--- install-chap-6.sh
+++ install-chap-6.sh
@@ -3,16 +3,22 @@
 set -e
 
 echo "Binutils"
+rm -rf binutils-2.36.1
 tar -xf binutils-2.36.1.tar.xz
 pushd binutils-2.36.1
 	mkdir -p build && cd build
-	../configure --prefix=$LFS/tools --with-sysroot=$LFS --target=$LFS_TGT -disable-nls --disable-werror
+	../configure --prefix=$LFS/tools \
+		--with-sysroot=$LFS \
+		--target=$LFS_TGT \
+		-disable-nls \
+		--disable-werror
 	make
 	make install
 popd
-rm -rf binutils-2.35
+rm -rf binutils-2.36.1
 
 echo "gcc"
+rm -rf gcc-10.2.0
 tar -xf gcc-10.2.0.tar.xz
 
 pushd gcc-10.2.0
@@ -22,16 +28,38 @@ pushd gcc-10.2.0
 	mv -v gmp-6.2.1 gmp
 	tar -xf ../mpc-1.2.1.tar.gz
 	mv -v mpc-1.2.1 mpc
-	sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+	sed -e '/m64=/s/lib64/lib/' \
+		-i.orig gcc/config/i386/t-linux64
 	mkdir -p build && cd build
-	../configure --target=$LFS_TGT --prefix=$LFS/tools --with-glibc-version=2.11 --with-sysroot=$LFS --with-newlib --without-headers --enable-initfini-array --disable-nls --disable-shared --disable-multilib --disable-decimal-float --disable-threads --disable-libatomic --disable-libgomp --disable-libquadmath --disable-libssp --disable-libvtv --disable-libstdcxx --enable-languages=c,c++
+	../configure \
+		--target=$LFS_TGT \
+		--prefix=$LFS/tools \
+		--with-glibc-version=2.11 \
+		--with-sysroot=$LFS \
+		--with-newlib \
+		--without-headers \
+		--enable-initfini-array \
+		--disable-nls \
+		--disable-shared \
+		--disable-multilib \
+		--disable-decimal-float \
+		--disable-threads \
+		--disable-libatomic \
+		--disable-libgomp \
+		--disable-libquadmath \
+		--disable-libssp \
+		--disable-libvtv \
+		--disable-libstdcxx \
+		--enable-languages=c,c++
 	make
 	make install
 	cd ..
-	cat gcc/limitx.h gcc/glimits.h gcc/limity.h > `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
+	cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+		`dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
 popd
 
 echo "Linux 5.10.17"
+rm -rf linux-5.10.17
 tar -xf linux-5.10.17.tar.xz
 pushd linux-5.10.17
 	make mrproper
@@ -42,37 +70,57 @@ pushd linux-5.10.17
 popd
 
 echo "glibc"
+rm -rf glibc-2.33
 tar -xf glibc-2.33.tar.xz
 pushd glibc-2.33
 	ln -sfv ../../lib/ld-linux-x86-64.so.2 $LFS/lib64
 	ln -sfv ../../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
-	patch -Np1 -i ../glibc-2.32-fhs-1.patch
+	patch -Np1 -i ../glibc-2.33-fhs-1.patch
 	mkdir -p build && cd build
-	../configure --prefix=/usr --host=$LFS_TGT --build=$(../scripts/config.guess) --enable-kernel=3.2 --with-headers=$LFS/usr/include libc_cv_slibdir=/lib
+	../configure \
+		--prefix=/usr \
+		--host=$LFS_TGT \
+		--build=$(../scripts/config.guess) \
+		--enable-kernel=3.2 \
+		--with-headers=$LFS/usr/include \
+		libc_cv_slibdir=/lib
 	make
 	make DESTDIR=$LFS install
 	$LFS/tools/libexec/gcc/$LFS_TGT/10.2.0/install-tools/mkheaders
 popd
 
+echo "gcc"
+rm -rf gcc-10.2.0
 tar -xf gcc-10.2.0.tar.xz
 pushd gcc-10.2.0
 	mkdir -p build && cd build
-	../libstdc++-v3/configure --host=$LFS_TGT --build=$(../config.guess) --prefix=/usr --disable-multilib --disable-nls --disable-libstdcxx-pch --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/10.2.0
+	../libstdc++-v3/configure \
+		--host=$LFS_TGT \
+		--build=$(../config.guess) \
+		--prefix=/usr \
+		--disable-multilib \
+		--disable-nls \
+		--disable-libstdcxx-pch \
+		--with-gxx-include-dir=/tools/$LFS_TGT/include/c++/10.2.0
 	make
 	make DESTDIR=$LFS install
 popd
 
 echo "m4"
+rm -rf m4-1.4.18
 tar -xf m4-1.4.18.tar.xz
 pushd m4-1.4.18
 	sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
 	echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
-	./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
+	./configure --prefix=/usr \
+		--host=$LFS_TGT \
+		--build=$(build-aux/config.guess)
 	make
 	make DESTDIR=$LFS install
 popd
 
 echo "ncurses"
+rm -rf ncurses-6.2
 tar -xf ncurses-6.2.tar.gz
 pushd ncurses-6.2
 	sed -i s/mawk// configure
@@ -82,7 +130,16 @@ pushd ncurses-6.2
 		make -C include
 		make -C progs tic
 	popd
-	./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess) --mandir=/usr/share/man --with-manpage-format=normal --with-shared --without-debug --without-ada --without-normal --enable-widec
+	./configure --prefix=/usr \
+		--host=$LFS_TGT \
+		--build=$(./config.guess) \
+		--mandir=/usr/share/man \
+		--with-manpage-format=normal \
+		--with-shared \
+		--without-debug \
+		--without-ada \
+		--without-normal \
+		--enable-widec
 	make
 	make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
 	echo "INPUT(-lncursesw)" > $LFS/usr/lib/libncurses.so
@@ -91,19 +148,28 @@ pushd ncurses-6.2
 popd
 
 echo "bash"
+rm -rf bash-5.1
 tar -xf bash-5.1.tar.gz
 pushd bash-5.1
-	./configure --prefix=/usr --build=$(support/config.guess) --host=$LFS_TGT --without-bash-malloc
+	./configure --prefix=/usr \
+		--build=$(support/config.guess) \
+		--host=$LFS_TGT \
+		--without-bash-malloc
 	make
 	make DESTDIR=$LFS install
 	mv $LFS/usr/bin/bash $LFS/bin/bash
 	ln -sv bash $LFS/bin/sh
 popd
 
-echo "coreutils" # 59
+echo "coreutils"
+rm -rf coreutils-8.32
 tar -xf coreutils-8.32.tar.xz
 pushd coreutils-8.32
-	./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess) --enable-install-program=hostname --enable-no-install-program=kill,uptime
+	./configure --prefix=/usr \
+		--host=$LFS_TGT \
+		--build=$(build-aux/config.guess) \
+		--enable-install-program=hostname \
+		--enable-no-install-program=kill,uptime
 	make
 	make DESTDIR=$LFS install
 	mv -v $LFS/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} $LFS/bin
@@ -117,6 +183,7 @@ pushd coreutils-8.32
 popd
 
 echo "diffutils"
+rm -rf diffutils-3.7
 tar -xf diffutils-3.7.tar.xz
 pushd diffutils-3.7
 	./configure --prefix=/usr --host=$LFS_TGT
@@ -125,22 +192,26 @@ pushd diffutils-3.7
 popd
 
 echo "file"
+rm -rf file-5.39
 tar -xf file-5.39.tar.gz
 pushd file-5.39
 	mkdir build
-	cd build
-	../configure --disable-bzlib \
-		--disable-libseccomp --disable-xzlib --disable-zlib
-	make
-	cd ..
+	pushd build
+		../configure --disable-bzlib \
+			--disable-libseccomp \
+			--disable-xzlib \
+			--disable-zlib
+		make
+	popd
 	./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
 	make FILE_COMPILE=$(pwd)/build/src/file
 	make DESTDIR=$LFS install
 popd
 
 echo "findutils"
-tar -xf findutils-4.7.0.tar.xz
-pushd findutils-4.7.0
+rm -rf findutils-4.8.0
+tar -xf findutils-4.8.0.tar.xz
+pushd findutils-4.8.0
 	./configure --prefix=/usr --host=$LFS_TGT --build=$(build-aux/config.guess)
 	make
 	make DESTDIR=$LFS install
@@ -149,23 +220,28 @@ pushd findutils-4.7.0
 popd
 
 echo "gawk"
+rm -rf gawk-5.1.0
 tar -xf gawk-5.1.0.tar.xz
 pushd gawk-5.1.0
 	sed -i 's/extras//' Makefile.in
-	./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
+	./configure --prefix=/usr \
+		--host=$LFS_TGT \
+		--build=$(./config.guess)
 	make
 	make DESTDIR=$LFS install
 popd
 
 echo "grep"
-tar -xf grep-3.4.tar.xz
-pushd grep-3.4
+rm -rf grep-3.6
+tar -xf grep-3.6.tar.xz
+pushd grep-3.6
 	./configure --prefix=/usr --host=$LFS_TGT --bindir=/bin
 	make
 	make DESTDIR=$LFS install
 popd
 
 echo "gzip"
+rm -rf gzip-1.10
 tar -xf gzip-1.10.tar.xz
 pushd gzip-1.10
 	./configure --prefix=/usr --host=$LFS_TGT
@@ -175,6 +251,7 @@ pushd gzip-1.10
 popd
 
 echo "make"
+rm -rf make-4.3
 tar -xf make-4.3.tar.gz
 pushd make-4.3
 	./configure --prefix=/usr \
@@ -186,6 +263,7 @@ pushd make-4.3
 popd
 
 echo "patch"
+rm -rf patch-2.7.6
 tar -xf patch-2.7.6.tar.xz
 pushd patch-2.7.6
 	./configure --prefix=/usr \
@@ -196,6 +274,7 @@ pushd patch-2.7.6
 popd
 
 echo "sed"
+rm -rf sed-4.8
 tar -xf sed-4.8.tar.xz
 pushd sed-4.8
 	./configure --prefix=/usr --host=$LFS_TGT --bindir=/bin
@@ -204,6 +283,7 @@ pushd sed-4.8
 popd
 
 echo "tar"
+rm -rf tar-1.34
 tar -xf tar-1.34.tar.xz
 pushd tar-1.34
 	./configure --prefix=/usr \
@@ -215,6 +295,7 @@ pushd tar-1.34
 popd
 
 echo "xz"
+rm -rf xz-5.2.5
 tar -xf xz-5.2.5.tar.xz
 pushd xz-5.2.5
 	./configure --prefix=/usr \
@@ -231,6 +312,7 @@ pushd xz-5.2.5
 popd
 
 echo "binutils"
+rm -rf binutils-2.36.1
 tar -xf binutils-2.36.1.tar.xz
 pushd binutils-2.36.1
 	mkdir -p build && cd build
